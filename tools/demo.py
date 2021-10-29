ee0c5e3db590f288d0107db319fd9e219c096575
yier <liyier90@gmail.com>
2021-10-29 17:17:00 +0800

    update demo args description, set up pkd custom run config, update .gitignore

--- tools/demo.py
+++ tools/demo.py
@@ -21,40 +21,41 @@ IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 
 def make_parser():
     parser = argparse.ArgumentParser("YOLOX Demo!")
-    parser.add_argument(
-        "demo", default="image", help="demo type, eg. image, video and webcam"
-    )
+    parser.add_argument("demo", default="image", help="demo type (image/video/webcam)")
     parser.add_argument("-expn", "--experiment-name", type=str, default=None)
     parser.add_argument("-n", "--name", type=str, default=None, help="model name")
 
     parser.add_argument(
-        "--path", default="./assets/dog.jpg", help="path to images or video"
+        "--path",
+        default="./assets/dog.jpg",
+        help="path to image/video/folder of images",
     )
-    parser.add_argument("--camid", type=int, default=0, help="webcam demo camera id")
+    parser.add_argument("--camid", type=int, default=0, help="webcam id")
     parser.add_argument(
         "--save_result",
         action="store_true",
-        help="whether to save the inference result of image/video",
+        help="flag for saving the inference result",
     )
 
-    # exp file
+    # Similar to yolov5/models/*.yaml?
     parser.add_argument(
         "-f",
         "--exp_file",
         default=None,
         type=str,
-        help="pls input your experiment description file",
+        help="experiment description file",
     )
     parser.add_argument("-c", "--ckpt", default=None, type=str, help="ckpt for eval")
     parser.add_argument(
         "--device",
         default="cpu",
         type=str,
-        help="device to run our model, can either be cpu or gpu",
+        help="device to run our model (cpu/gpu)",
     )
-    parser.add_argument("--conf", default=0.3, type=float, help="test conf")
-    parser.add_argument("--nms", default=0.3, type=float, help="test nms threshold")
-    parser.add_argument("--tsize", default=None, type=int, help="test img size")
+    parser.add_argument("--conf", default=0.3, type=float, help="conf threshold")
+    # IOU threshold
+    parser.add_argument("--nms", default=0.3, type=float, help="nms threshold")
+    parser.add_argument("--tsize", default=None, type=int, help="img size")
     parser.add_argument(
         "--fp16",
         dest="fp16",
@@ -159,8 +160,11 @@ class Predictor(object):
             if self.decoder is not None:
                 outputs = self.decoder(outputs, dtype=outputs.type())
             outputs = postprocess(
-                outputs, self.num_classes, self.confthre,
-                self.nmsthre, class_agnostic=True
+                outputs,
+                self.num_classes,
+                self.confthre,
+                self.nmsthre,
+                class_agnostic=True,
             )
             logger.info("Infer time: {:.4f}s".format(time.time() - t0))
         return outputs, img_info
@@ -299,8 +303,14 @@ def main(exp, args):
         decoder = None
 
     predictor = Predictor(
-        model, exp, COCO_CLASSES, trt_file, decoder,
-        args.device, args.fp16, args.legacy,
+        model,
+        exp,
+        COCO_CLASSES,
+        trt_file,
+        decoder,
+        args.device,
+        args.fp16,
+        args.legacy,
     )
     current_time = time.localtime()
     if args.demo == "image":
@@ -311,6 +321,6 @@ def main(exp, args):
 
 if __name__ == "__main__":
     args = make_parser().parse_args()
-    exp = get_exp(args.exp_file, args.name)
+    exp_obj = get_exp(args.exp_file, args.name)
 
-    main(exp, args)
+    main(exp_obj, args)
